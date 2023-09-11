d88e20dcb75616f7c52e91f5578c0e7114fd494a
endloww <aurelien.blanchet74@gmail.com>
2023-09-11 14:42:40 +0200

    ADD: Ajout des constructeurs dans la classe Souris

--- src/Souris.java
+++ src/Souris.java
@@ -1,24 +1,60 @@
 public class Souris {
 
-    private int age;
-    private float poids;
+    private int age; // en mois
+    private float poids; // en gramme
     private String couleur;
+    private boolean clone;
+    private int EsperanceVie;
 
-    public int EsperanceVie = 36;
+    public static final int ESPERANCE_VIE_DEFAUT = 36;
 
     public Souris(float poids, String couleur) {
         this.poids = poids;
         this.couleur = couleur;
+        this.age = 0 ;
+        this.EsperanceVie = 36;
+        this.clone = false;
+
+        System.out.println("Une nouvelle souris !");
     }
 
-    public Souris(int age, float poids, String couleur){
-        this.age = age;
-        this.poids = poids;
-        this.couleur = couleur;
-        System.out.println("nouvelle souris créée");
-    
+    public Souris(int unAge, float unPoids, String uneCouleur){
+        this.age = unAge;
+        this.poids = unPoids;
+        this.couleur = uneCouleur;
+        this.EsperanceVie = 36;
+        this.clone = false;
+
+        System.out.println("Une nouvelle souris !");
                 
     }
+    public Souris(Souris sourisClonee){
+        this.age = sourisClonee.getAge();
+        this.couleur = sourisClonee.getCouleur();
+        this.poids = sourisClonee.getPoids();
+        this.EsperanceVie = sourisClonee.getEsperanceVie() * (4/5);
+        
+        System.out.println("Clone d'une souris !");
+    }
+        
+
+    public int getEsperanceVie() {
+        return EsperanceVie;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public float getPoids() {
+        return poids;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+
 
     public void vieillir() {
     }
